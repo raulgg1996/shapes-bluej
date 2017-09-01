c04a694a9db7d7a4031d6120b4dfc5aa6d14aced
raul <chavo_cruzazul@hotmail.com>
2017-08-31 23:37:09 -0500

    Se concluyo el ejercicio de manera correcta,ademas se documento

--- Casa.java
+++ Casa.java
@@ -1,5 +1,11 @@
+
+/*
+ * @author Raul Gonzalez Gonzalez
+   @version 3.5.1
+ */
 class Casa
 {
+   
     //variables de instancia
     Circle ventana;
     Triangle techo;
@@ -11,15 +17,48 @@ class Casa
        pared=new Square();
        puerta=new Square();
     }
+    /*metodo que permite dibujar cada uno de los elementos que conforman la 
+     * casa.
+     */
     void dibujate(){
          ventana.makeVisible();
          techo.makeVisible();
          pared.makeVisible();
          puerta.makeVisible();
     }
+    /*
+     * metodo que permite acomodar los elementos que conforman una casa para
+     * poder representar la misma.ademas de cambiar aspectos de la casa.
+     */
     void acomodate(){
-         
+       techo.moveHorizontal(100);
+       techo.changeSize(60,60);
+       techo.moveVertical(50);
+       techo.changeColor("black");
+       ventana.moveHorizontal(120);
+       ventana.changeSize(20);
+       ventana.moveVertical(40);
+       ventana.changeColor("blue");
+       pared.moveHorizontal(60);
+       pared.changeSize(60);
+       pared.moveVertical(75);
+       pared.changeColor("green");
+       puerta.moveHorizontal(75);
+       puerta.changeColor("red");
+       puerta.changeSize(30);
+       puerta.moveVertical(105);
     
     
+    }
+    /*
+     * metodo que permite mostrar una segyunda casa en diferente posicion a
+     * la primer casa mostrada.
+     */
+    void mueveIzquierda()
+    {
+        pared.moveHorizontal(100);
+        techo.moveHorizontal(100);
+        puerta.moveHorizontal(100);
+        ventana.moveHorizontal(100);
     }
 }    
